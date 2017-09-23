06cacb542e4c21432fd1fa110c5f2172781c55c3
wma <wma@thoughtworks.com>
2017-09-23 14:25:35 +0800

    add login logout feature

--- app.rb
+++ app.rb
@@ -1,10 +1,39 @@
 require 'sinatra'
 require 'sinatra/json'
 require 'rubygems'
+require 'digest'
 require './models/models.rb'
+require './utils.rb'
+
+enable :sessions
 
 get '/' do
-  'Hello world!'
+  redirect to(:login)
+end
+
+get '/login' do
+  return_page(:'login/login')
+end
+
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+  return_page(:'login/login', 'Login failed!') unless is_valid_input_for_login(username, password)
+
+  encrypted_password = Digest::SHA512.hexdigest password
+  user_in_db = User.first(:username => username, :password => encrypted_password)
+
+  if user_in_db == nil
+    return_page(:'login/login', 'Login failed!')
+  else
+    session[:current_user] = user_in_db
+    redirect to(:users)
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect to(:login)
 end
 
 get '/create-user' do
