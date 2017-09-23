1fa4fae2c210c8f5d1aec76634a1a495a586a6c9
wma <wma@thoughtworks.com>
2017-09-23 17:27:17 +0800

    check duplicate username for register process

--- controllers/register_controller.rb
+++ controllers/register_controller.rb
@@ -12,6 +12,10 @@ post '/register' do
     halt erb(:'register/new', :layout => :main, :locals => {:message => '注册失败，' + message})
   end
 
+  if is_duplicate(username)
+    halt erb(:'register/new', :layout => :main, :locals => {:message => '注册失败，该手机号已存在，请直接登录'})
+  end
+
   encrypted_password = Digest::SHA512.hexdigest password1
   user = User.new(:username => username, :password => encrypted_password)
   user.save
@@ -31,5 +35,10 @@ helpers do
     return false, '手机号和密码长度超限' if username.length > 11 || password1.length > 20 || password1.length > 20
     return false, '两次输入的密码不一致' if password1 != password2
     return true, ''
+  end
+
+  def is_duplicate(username)
+    user = User.first(:username => username)
+    return user != nil
   end
 end
