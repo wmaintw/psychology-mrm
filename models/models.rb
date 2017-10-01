9f4338985d6c57cdc199a56a1d7d3373fa62be08
wma <wma@thoughtworks.com>
2017-10-01 15:04:54 +0800

    add description to db migration methods

--- models/models.rb
+++ models/models.rb
@@ -8,11 +8,15 @@ class User
   include DataMapper::Resource
 
   property :id,         Serial
-  property :username,   String
+  property :username,   String, :length => 32
   property :password,   String, :length => 128
   property :name,       String
 end
 
 DataMapper.finalize
+
+# This will drop existing table schema first, so that data will be wiped.
 # DataMapper.auto_migrate!
+
+# This just update table schema, add new tables or columns, but do nothing about modify columns.
 DataMapper.auto_upgrade!
