31a00ac99ddb68b2c4ff3775127d69e098d41a01
wma <wma@thoughtworks.com>
2017-10-09 16:08:38 +0800

    add Lead domain object

--- models/models.rb
+++ models/models.rb
@@ -13,6 +13,35 @@ class User
   property :name,       String
 end
 
+class Lead
+  include DataMapper::Resource
+
+  property :id,                     Serial
+  property :mobile,                 String, :length => 18
+  property :initial_contact_at      DateTime
+  property :name,                   String, :length => 12
+  property :gender,                 String, :length => 4
+  property :statement_by_visitor    Text
+  property :initial_problem         Text
+  property :respond_to_problem      Text
+  property :affected_by_problem     Text
+
+  property :age,                    Integer
+  property :marital_status,         String
+  property :has_children,           String
+  property :ethnic                  String
+  property :occupation              String
+  property :education               String
+
+  property :referral                Boolean, :default => false
+  property :referral_comments       Text
+  property :comments                Text
+
+  property :booking_sms_sent        Boolean, :default => false
+  property :status                  String
+  property :active                  Boolean, :default => true
+end
+
 DataMapper.finalize
 
 # This will drop existing table schema first, so that data will be wiped.
